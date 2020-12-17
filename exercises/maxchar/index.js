8a37c982cabc143270e38de690725aa8a386f651
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-12-17 19:15:48 +0530

    solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let obj = {};
+    let max = 0;
+    let maxChar = '';
+    //Use for-of for arrays or strings only, not objects
+    for (let char of str) {
+        // obj[char] = obj[char] + 1 || 1; // this line is similar to the if-else below
+        if(obj[char]) {
+            obj[char] + 1;
+        } else {
+            obj[char] = 1;
+        }
+    }
+    // Always use for-in to iterate over an object
+    for (let char in obj) {
+        if (obj[char] > max) {
+            max = obj[char];
+            maxChar = char;
+    }
+    return maxChar;
+    }
+}
 
 module.exports = maxChar;
