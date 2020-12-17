d8c53d11b81cc23171ffd06b185d2432c6c3973a
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-12-17 18:22:55 +0530

    solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    //solution-1
+    //toString() converts into a string
+    //parseInt converts into integer
+    //Math.sign(n) returns 1 for +ve n, etc...
+    const reversed = n.toString().split('').reverse().join('');
+    return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
