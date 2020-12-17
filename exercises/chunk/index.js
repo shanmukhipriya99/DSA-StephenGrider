4d7f06c613d77e1690851fdfd23237a15c08e178
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-12-17 21:57:12 +0530

    solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    //solution-1
+    let chunked = [];
+    for (let element of array) {
+        let last = chunked[chunked.length - 1];
+        if (!last || last.length === size) {
+            chunked.push([element]);
+        } else {
+            last.push(element);   //automatically gets updated in the chunked array
+        }
+    }
+    return chunked;
+}
 
 module.exports = chunk;
